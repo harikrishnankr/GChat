bc7b4951324b5e0b5088dfc4d9de7d7cb9af478a
krhari <krhari@LAP-1723>
2020-12-24 18:08:29 +0530

    Added laoder

--- src/route.tsx
+++ src/route.tsx
@@ -1,12 +1,13 @@
 import React, { Component, lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Loader from './shared/loader';
 
 const LoginComponent = lazy(() => import(`./modules/auth/login`))
 
 class RouteComponent extends Component {
     render() {
         return (
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<Loader />}>
                 <Switch>
                     <Route exact path="/" component={LoginComponent} />
                 </Switch>
