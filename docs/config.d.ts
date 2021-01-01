332d83dff3e6cd315e7d52e66221ca1bfd2dc131
krhari <krhari@LAP-1723>
2021-01-01 18:46:08 +0530

    Build version 0.0.1

--- docs/config.d.ts
+++ docs/config.d.ts
@@ -1,3 +1,3 @@
 export declare const G_AUTH_CLIENT_ID = "995927193268-5eo1colbv3epna341ulqqosfr42tm9cn.apps.googleusercontent.com";
 export declare const G_AUTH_CLIENT_SECRET = "odjfeP3zpLY2GdohwyE2SnMY";
-export declare const APP_ROUTE = "http://localhost:8000";
+export declare const APP_ROUTE = "https://gchat-app-server.herokuapp.com";
