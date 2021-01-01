7cc43c6bc8c5e1177e525e290585392d21dda25f
krhari <krhari@LAP-1723>
2021-01-01 18:42:48 +0530

    Updated the config for live connection

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const G_AUTH_CLIENT_ID = '995927193268-5eo1colbv3epna341ulqqosfr42tm9cn.apps.googleusercontent.com';
 export const G_AUTH_CLIENT_SECRET = 'odjfeP3zpLY2GdohwyE2SnMY';
-export const APP_ROUTE = 'http://localhost:8000';
+export const APP_ROUTE = 'https://gchat-app-server.herokuapp.com';
+// export const APP_ROUTE = 'http://localhost:8000';
